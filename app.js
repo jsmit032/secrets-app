8be46e8a421398330865333f75d97e3f116d14ca
Jennifer <jsmit032@ucr.edu>
2020-04-06 13:58:25 -0500

    Level 1 Security Only - Username and Password Only - no encryption

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model('User', userSchema);
 
 // Routes
 app.get('/', function(req, res){
@@ -18,11 +28,58 @@ app.get('/register', function(req, res){
   res.render('register');
 });
 
+app.post('/register', function(req, res){
+  const userName = req.body.username;
+  const password = req.body.password;
+
+  var newUser = new User({
+    email: userName,
+    password: password
+  });
+
+  User.findOne({email: userName}, function(err, foundUser){
+    if (!err) {
+      if (!foundUser) {
+        newUser.save(function(err){
+          if (!err)
+            {
+              res.render('secrets');
+            } else {
+              console.log(err);
+            }
+        });
+      } else {
+        res.redirect('/register');
+        console.log("There's already a user with this e-mail. Please provide a different e-mail address!");
+      }
+    }
+  });
+});
+
 app.get('/login', function(req, res){
   res.render('login');
 });
 
+app.post('/login', function(req, res){
+  const userName = req.body.username;
+  const password = req.body.password;
 
+  User.findOne({email: userName}, function(err, foundUser){
+    if (!err) {
+      if (!foundUser) {
+        console.log("This user doesn't exisit, please register to login");
+      } else {
+        if (password === foundUser.password) {
+          res.render('secrets');
+        } else {
+          console.log("Either username or password is incorrect, please try again.");
+        }
+      }
+    } else {
+      console.log(err);
+    }
+  });
+});
 
 
 app.listen(3000, function(){
