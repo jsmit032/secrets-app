c1fc78d2110ed7d5af3e740502715fb376443328
Jennifer <jsmit032@ucr.edu>
2020-04-07 14:16:09 -0500

    add login using google account, not yet storing data to local DB

--- app.js
+++ app.js
@@ -41,8 +41,15 @@ const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENTID,
@@ -51,6 +58,7 @@ passport.use(new GoogleStrategy({
     userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
   },
   function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
@@ -60,6 +68,17 @@ passport.use(new GoogleStrategy({
 // Routes
 app.get('/', function(req, res){
   res.render('home');
+});
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, to secrests.
+    res.redirect('/secrets');
 });
 
 app.get('/register', function(req, res){
