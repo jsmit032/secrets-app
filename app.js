a68bd36799fe498701dbedaac9f85c954e6ba741
Jennifer <jsmit032@ucr.edu>
2020-04-07 14:19:01 -0500

    idenify registered users by their googleId

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
